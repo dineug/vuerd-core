d4d2deb2481a5ec4f9e0f7fe500a952ef007180a
dineug <dineug2@gmail.com>
2019-05-20 03:30:14 +0900

    add pug, rxjs

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,10 @@
 import Vue from 'vue';
 import App from './App.vue';
 import store from './store';
+import VueRx from 'vue-rx';
 
 Vue.config.productionTip = false;
+Vue.use(VueRx);
 
 new Vue({
   store,
